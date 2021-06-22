4f0f382e3edaec20d0e938d8250e5f0b5b061979
leyao8181 <271078181@qq.com>
2021-06-22 23:25:39 +0800

    fourth commit

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -11,6 +11,6 @@ public class K8SController {
 
     @RequestMapping("/k8s")
     public String k8s(){
-        return "hello K8s and  rhett <br/>";
+        return "hello K8s and t <br/>";
     }
 }

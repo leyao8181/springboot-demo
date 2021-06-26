ea8a2732e8ef367d180e24bfd7222d5a9e41cda2
leyao8181 <271078181@qq.com>
2021-06-26 18:08:42 +0800

    okokok

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -11,6 +11,6 @@ public class K8SController {
 
     @RequestMapping("/k8s")
     public String k8s(){
-        return "hello K8s and ok ok <br/>";
+        return "hello K8s and ok ok ok<br/>";
     }
 }
